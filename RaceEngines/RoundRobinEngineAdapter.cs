6bf6b1f1797d8895607a5f1f2791b86d501e2b36
Stewart McMillan <stewmac570@gmail.com>
2025-07-07 08:11:28 +1000

    Rename RoundRobinMatchResult.cs to RoundRobinMatch.cs and update references

---
+++ RaceEngines/RoundRobinEngineAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RCDragManagerProd.RaceEngines
+{
+    public class RoundRobinEngineAdapter : IRaceEngine
+    {
+        private readonly RoundRobinEngine _engine = new RoundRobinEngine();
+
+        public void LoadDrivers(List<Driver> drivers) => _engine.LoadDrivers(drivers);
+
+        public void GenerateBracket() => _engine.GenerateMatches();
+
+        public void Reset() => _engine.Reset();
+
+        public IReadOnlyList<EngineMatch> GetMatches()
+        {
+            return _engine.GetMatches()
+                          .Select(m => new EngineMatch
+                          {
+                              MatchId = m.MatchId,
+                              Driver1 = m.D1,   // <-- Use D1 not Driver1
+                              Driver2 = m.D2,   // <-- Use D2 not Driver2
+                              RoundLabel = m.RoundLabel,
+                              HasResult = _engine.HasWinner(m.MatchId)
+                          })
+                          .ToList();
+        }
+
+
+        public IReadOnlyList<string> GetRoundOrder() => _engine.GetRoundLabels();
+
+        public void SetWinner(int matchId, Driver winner) => _engine.SetWinner(matchId, winner);
+
+        public bool HasWinner(int matchId) => _engine.HasWinner(matchId);
+    }
+
+}
---
+++ RoundRobinMatch.cs
@@ -0,0 +1,9 @@
+﻿using RCDragManagerProd;
+
+public class RoundRobinMatch
+{
+    public int MatchId { get; set; }
+    public Driver Driver1 { get; set; }
+    public Driver Driver2 { get; set; }
+    public string RoundLabel { get; set; } // "R1", "R2", etc.
+}
