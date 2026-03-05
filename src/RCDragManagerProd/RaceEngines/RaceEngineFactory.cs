ea504a327fcb336c74feba021be0c557980a9ab9
Stew Mac <39642055+stewmac570@users.noreply.github.com>
2026-03-06 05:48:18 +1000

    Cleanup: remove unused using in RaceEngineFactory

--- src/RCDragManagerProd/RaceEngines/RaceEngineFactory.cs
+++ src/RCDragManagerProd/RaceEngines/RaceEngineFactory.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 
 using RCDragManagerProd.Domain;
-using RCDragManagerProd.ViewModels;    // only if returning VM rows
 using RCDragManagerProd.Logging;      // Logger
 
 
@@ -38,7 +37,7 @@ namespace RCDragManagerProd.RaceEngines
 
                 default:
                     Logger.Log($"[ENGINE FACTORY] Unknown race type '{cleanType}'");
-                    throw new ArgumentException($"Unknown race type “{raceType}”.", nameof(raceType));
+                    throw new ArgumentException($"Unknown race type \"{raceType}\".", nameof(raceType));
             }
         }
     }
